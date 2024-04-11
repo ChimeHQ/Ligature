fb4c3c6374e667615add6f970745aec3dbe79771
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-04-11 07:18:02 -0400

    Package skeleton

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+	name: "Ligature",
+	platforms: [
+		.macOS(.v10_15),
+		.iOS(.v13),
+		.tvOS(.v13),
+		.watchOS(.v6),
+		.macCatalyst(.v13),
+		.visionOS(.v1)
+	],
+	products: [
+		.library(name: "Ligature", targets: ["Ligature"]),
+	],
+	targets: [
+		.target(name: "Ligature"),
+		.testTarget(name: "LigatureTests", dependencies: ["Ligature"]),
+	]
+)
+
+let swiftSettings: [SwiftSetting] = [
+	.enableExperimentalFeature("StrictConcurrency"),
+	.enableUpcomingFeature("DisableOutwardActorInference"),
+	.enableUpcomingFeature("IsolatedDefaultValues"),
+]
+
+for target in package.targets {
+	var settings = target.swiftSettings ?? []
+	settings.append(contentsOf: swiftSettings)
+	target.swiftSettings = settings
+}
