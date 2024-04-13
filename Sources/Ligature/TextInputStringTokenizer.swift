cfd6a0989cfe919d80e9c4abb5b367313e723d79
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-04-13 06:51:43 -0400

    starting on some AppKit support

--- Sources/Ligature/TextInputStringTokenizer.swift
+++ Sources/Ligature/TextInputStringTokenizer.swift
@@ -38,7 +38,40 @@ extension TextInputStringTokenizer : TextTokenizer {
 	}
 
 	public func isPosition(_ position: Position, atBoundary granularity: TextGranularity, inDirection direction: TextDirection) -> Bool {
-		return false
+        guard let position = position as? UTF16TextPosition else { return false }
+
+        let string = (textInput.attributedString().string as NSString)
+        let forward = textInput.isForwardDirection(direction)
+        let start = forward ? max(position.value - 1, 0) : 0
+        let end = forward ? string.length : min(position.value + 1, string.length)
+        let options: NSString.EnumerationOptions
+
+        switch granularity {
+        case .character:
+            options = [.byComposedCharacterSequences]
+        case .word:
+            options = [.byWords]
+        case .line:
+            options = [.byLines]
+        case .sentence:
+            options = [.bySentences]
+        case .paragraph:
+            options = [.byParagraphs]
+        case .document:
+            return false
+        }
+
+        var atBoundary = false
+
+        string.enumerateSubstrings(in: NSRange(start..<end), options: options) { _, subRange, _, stop in
+            let boundary = forward ? subRange.upperBound : subRange.lowerBound
+
+            atBoundary = position.value == boundary
+
+            stop.pointee = true
+        }
+
+		return atBoundary
 	}
 
 	public func isPosition(_ position: Position, withinTextUnit granularity: TextGranularity, inDirection direction: TextDirection) -> Bool {
